c88cc4223372e152ed0d8f857c126f7582f1c77d
masomasharifi <massharifi@gmail.com>
2021-07-29 09:13:14 -0700

    Walmart

---
+++ src/test/java/Walmart/bdd/framework/Walmart/bdd/framework/AppTest.java
@@ -0,0 +1,20 @@
+package Walmart.bdd.framework.Walmart.bdd.framework;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
